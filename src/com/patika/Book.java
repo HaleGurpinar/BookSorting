dc5b83523c6a7706c3230b629d7e13b8c1c78cf5
HaleGurpinar <106753721+HaleGurpinar@users.noreply.github.com>
2023-05-31 15:58:30 +0300

    Unnecess.

--- src/com/patika/Book.java
+++ src/com/patika/Book.java
@@ -27,31 +27,18 @@ public class Book implements Comparable<Book>{
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public int getPageNumber() {
         return pageNumber;
     }
 
-    public void setPageNumber(int pageNumber) {
-        this.pageNumber = pageNumber;
-    }
 
     public String getAuthor() {
         return author;
     }
 
-    public void setAuthor(String author) {
-        this.author = author;
-    }
 
     public String  getPublishDate() {
         return publishDate;
     }
 
-    public void setPublishDate(String publishDate) {
-        this.publishDate = publishDate;
-    }
 }
