58d54879ae160162d31d235917eb5e0520776f88
HaleGurpinar <106753721+HaleGurpinar@users.noreply.github.com>
2023-06-01 14:33:33 +0300

    LeagueTable Fixturing

--- src/com/patika/LeagueTable/League.java
+++ src/com/patika/LeagueTable/League.java
@@ -1,7 +1,6 @@
 package com.patika.LeagueTable;
 
 import java.util.ArrayList;
-import java.util.Locale;
 import java.util.Scanner;
 
 public class League {
@@ -11,6 +10,7 @@ public class League {
     ArrayList<String> teams= new ArrayList<>();
     ArrayList<String> firstHalfFixture= new ArrayList<>();
     ArrayList<String> secondHalfFixture= new ArrayList<>();
+    ArrayList<Integer> list = new ArrayList<>();
 
     public League() {
     }
@@ -27,25 +27,79 @@ public class League {
         this.team = team;
     }
 
-    public void generateFixture(){
+    public void generateFixture() {
         System.out.println("-------------------Super League--------------------");
-        Scanner sc= new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter teams number");
-        int n= sc.nextInt();
-        for (int i=0;i<n;i++){
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
             System.out.println("Enter team name");
-            String t=sc.next();
+            String t = sc.next();
             teams.add(t.toUpperCase());
         }
    /*for (String s: teams){
        System.out.println(s);
    }*/
-        if(n%2!=0){
+        if (n % 2 != 0) {
             teams.add("BAY");
         }
-       /* for (String s: teams){
-            System.out.println(s);
-        }*/
+        for (String t : teams) {
+            System.out.println(t);
+        }
+        try {
+            int round = 1;
+            while (round >= 2 * (teams.size() - 1)) {
+                System.out.println("Round " + round);
+
+                for (int i = 0; i < teams.size() / 2; i++) {
+                    int home = (int) (Math.random() * teams.size());
+                    while (list.contains(home)) {
+                        home = (int) (Math.random() * teams.size());
+                    }
+                    int away = (int) ((Math.random() * teams.size()));
+                    while (away == home || list.contains(away)) {
+                        away = (int) ((Math.random() * teams.size()));
+                    }
+
+                    String fiksComb = home + "" + away;
+
+                    while (firstHalfFixture.contains(fiksComb)) {
+                        home = (int) ((Math.random() * teams.size()));
+
+                        while (list.contains(home)) {
+                            home = (int) ((Math.random() * teams.size()));
+                        }
+
+                        away = (int) ((Math.random() * teams.size()));
+
+                        while (away == home || list.contains(away)) {
+                            away = (int) ((Math.random() * teams.size()));
+                        }
+                        fiksComb = home + "" + away;
+                    }
+                    list.add(home);
+                    list.add(away);
+                    firstHalfFixture.add(fiksComb);
+                    String match = teams.get(home) + " VS " + teams.get(away);
+                    secondHalfFixture.add(match);
+                }
+                for (Object l : secondHalfFixture) {
+                    System.out.println(l);
+                }
+
+
+                System.out.println("------------------");
+                list.clear();
+                secondHalfFixture.clear();
+                round++;
+            }
+        }catch (Exception e ){
+            System.out.println("Error Received !!!");
+        }
+
+
+        System.out.println("end");
+
     }
 
 }
--- src/com/patika/LeagueTable/Main.java
+++ src/com/patika/LeagueTable/Main.java
@@ -1,9 +1,108 @@
 package com.patika.LeagueTable;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-League league=new League();
-league.generateFixture();
+
+
+        ArrayList<String> firstHalfFixture = new ArrayList<>();
+        ArrayList<String> secondHalfFixture = new ArrayList<>();
+        ArrayList<Integer> list = new ArrayList<>();
+
+        Scanner sc = new Scanner(System.in);
+        List<String> teams = new ArrayList<>();
+        System.out.println("-------------------Super League--------------------");
+
+        int n=0;
+        try {
+            System.out.println("Enter number of teams : ");
+             n = sc.nextInt();
+        }catch (Exception e){
+            System.out.println("Please Enter of number !!!");
+        }
+        try{
+            for (int i = 0; i < n; i++) {
+                System.out.println("Enter team name : ");
+                String t = sc.next();
+                teams.add(t.toUpperCase());
+            }
+
+            if (n % 2 != 0) {
+                teams.add("BAY");
+            }
+        }catch (Exception e) {
+            System.out.println("You Entered Wrong Value !!!!");
+        }
+        if (teams.size() != 0) {
+
+            System.out.println("The Teams\n");
+
+            for (String t : teams) {
+            System.out.println(t);
+        }
+            System.out.println("#######################################");
+        } else {
+            System.out.println("Enter the number of teams to play !!!");
+        }
+        try {
+            int round = 1;
+            while (round <= 2 * (teams.size() - 1)) {
+                System.out.println("Round " + round);
+
+                for (int i = 0; i < teams.size() / 2; i++) {
+                    int home = (int) (Math.random() * teams.size());
+                    while (list.contains(home)) {
+                        home = (int) (Math.random() * teams.size());
+                    }
+                    int away = (int) ((Math.random() * teams.size()));
+                    while (away == home || list.contains(away)) {
+                        away = (int) ((Math.random() * teams.size()));
+                    }
+
+                    String fiksComb = home + "" + away;
+
+                    while (firstHalfFixture.contains(fiksComb)) {
+                        home = (int) ((Math.random() * teams.size()));
+
+                        while (list.contains(home)) {
+                            home = (int) ((Math.random() * teams.size()));
+                        }
+
+                        away = (int) ((Math.random() * teams.size()));
+
+                        while (away == home || list.contains(away)) {
+                            away = (int) ((Math.random() * teams.size()));
+                        }
+                        fiksComb = home + "" + away;
+                    }
+                    list.add(home);
+                    list.add(away);
+                    firstHalfFixture.add(fiksComb);
+                    String match = teams.get(home) + " VS " + teams.get(away);
+                    secondHalfFixture.add(match);
+                }
+                for (Object l : secondHalfFixture) {
+                    System.out.println(l);
+                }
+
+
+                System.out.println("------------------");
+                list.clear();
+                secondHalfFixture.clear();
+                round++;
+            }
+        } catch (Exception e) {
+            System.out.println("Error Received !!!");
+        }
+
+
+        System.out.println("end");
+
     }
+
+
 }
